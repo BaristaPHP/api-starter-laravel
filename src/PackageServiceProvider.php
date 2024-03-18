8d1762b345aaddc801d7ee1c1ac320caad38916e
Mohamed Gamal <mg.dev1992@icloud.com>
2024-03-19 00:16:54 +0200

    Initial commit

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vendor\Package;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
